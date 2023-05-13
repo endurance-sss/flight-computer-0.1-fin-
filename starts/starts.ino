9aec55bf91f9d35c224f76a156ecff7c9ad58273
rowingdude <49796265+rowingdude@users.noreply.github.com>
2023-05-13 13:13:26 -0400

    Update starts.ino
    In this revision, I consolidated pin layouts, objects, and used branchless operations to reduce the number of conditional statements

--- starts/starts.ino
+++ starts/starts.ino
@@ -1,26 +1,28 @@
-#include <MPU6050_tockn.h> //scl =A5 / sda =A4 /vcc =5v / grn =grn
+
+
+#include <MPU6050_tockn.h>
 #include <Servo.h>
 
-int ledping =7;  //led pin
-int ledpinr =6;
+// Pin Definitions
+const int LED_PIN_G = 7;
+const int LED_PIN_R = 6;
+const int BUZZER_PIN = 5;
+const int SERVO_PIN_1 = 9;
+const int SERVO_PIN_2 = 10;
+const int SERVO_PIN_3 = 11;
+const int SERVO_PIN_4 = 12;
 
-int buzzer  =5; // buzzer pin 
-
-int servopin1 = 9;  // Servo pin
-int servopin2 = 10;
-int servopin3 = 11;
-int servopin4 = 12;
-
-Servo servo1;   // Servo object 
+// Object Declarations
+Servo servo1;
 Servo servo2;
 Servo servo3;
 Servo servo4;
 
-int pos = 0; //position
-
 MPU6050 mpu6050(Wire);
 
-long timer = 0;
+// Time Variables
+unsigned long timer = 0;
+const unsigned long interval = 1000;
 
 void setup() {
   Serial.begin(9600);
@@ -28,57 +30,40 @@ void setup() {
   mpu6050.begin();
   mpu6050.calcGyroOffsets(true);
 
-  servo1.attach(servopin1);   //attach object to pin 
-  servo2.attach(servopin2);
-  servo3.attach(servopin3);
-  servo4.attach(servopin4);
+  servo1.attach(SERVO_PIN_1);
+  servo2.attach(SERVO_PIN_2);
+  servo3.attach(SERVO_PIN_3);
+  servo4.attach(SERVO_PIN_4);
 
-  pinMode(7,OUTPUT);
-  pinMode(6,OUTPUT);
-  pinMode(5,OUTPUT);
+  pinMode(LED_PIN_G, OUTPUT);
+  pinMode(LED_PIN_R, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
 }
 
 void loop() {
-
-  digitalWrite(ledpinr, HIGH);
-  digitalWrite(ledping, HIGH);
-  digitalWrite(buzzer, HIGH);
+  digitalWrite(LED_PIN_R, HIGH);
+  digitalWrite(LED_PIN_G, HIGH);
+  digitalWrite(BUZZER_PIN, HIGH);
   mpu6050.update();
 
-  if(millis() - timer > 1000){
-    
-    
-    Serial.print("angleX : ");            // print 
+  if (millis() - timer > interval) {
+    Serial.print("angleX: ");
     Serial.print(mpu6050.getAngleX());
-    Serial.print("  angleY : ");
+    Serial.print("  angleY: ");
     Serial.println(mpu6050.getAngleY());
 
     timer = millis();
-
   }
-      
-       //x-axis / roll
-      
-  if( mpu6050.getAngleX() == -1<0< 1){                  
-   servo2.write(90);    //set to 90 degrees
-    }
-   servo2.write(90 - mpu6050.getAngleY()); //unstable angle
-   
-   if( mpu6050.getAngleX() == 0){
-      servo1.write(90); //set to 90 degrees
-    }
-   servo1.write(90 + mpu6050.getAngleY()); //unstable angle
 
-       //y-axis / pitch 
-       
-   if(mpu6050.getAngleY() == 0){        
-    servo3.write(90);//set to 90 degrees
-    }
-    servo3.write(90 +mpu6050.getAngleX()); //unstable angle
-    
-   if(mpu6050.getAngleY() == 0){
-    servo4.write(90); //set to 90 degrees
-    }
-    servo4.write(90 - mpu6050.getAngleX());//unstable angle
-  
-  }
+  // x-axis / roll
+  int angleX = mpu6050.getAngleX();
+  int angleY = mpu6050.getAngleY();
+  servo2.write(90 - angleY); // unstable angle
+  servo1.write(90 + angleY); // unstable angle
+
+  // y-axis / pitch
+  servo3.write(90 + angleX); // unstable angle
+  servo4.write(90 - angleX); // unstable angle
+
+  delay(10); // Small delay to reduce processing load
+}
